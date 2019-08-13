f827f613c1834a07a86a7450d10a391fa045f581
joseblotta <52714385+joseblotta@users.noreply.github.com>
2019-08-13 17:01:52 -0400

    Add files via upload

---
+++ assets/data/bookData.ts
@@ -0,0 +1,16 @@
+import { Book } from '../../app/book';
+
+export const BOOKDATA:Book[] = [
+    {author:"Bob",
+    country:"Canada",
+    category:"Terror"    
+    },
+    {author:"Mark",
+    country:"USA",
+    category:"Romance"    
+    },
+    {author:"Ralph",
+    country:"Brasil",
+    category:"Cience"    
+    },
+]
